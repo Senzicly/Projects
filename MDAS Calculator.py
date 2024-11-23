e6f147b5df4b96b92ae1a6cd44fef030f90633a4
Senzicly <luismanueldacostar@gmail.com>
2024-11-23 16:29:33 -0500

    Update MDAS Calculator.py
    1. No longer gives an error when dividing by zero, just makes you do it again.
    
    2. No longer gives an error if you don't input a number, just makes you do it again.
    
    3. Made the code look less redundant.

--- MDAS Calculator.py
+++ MDAS Calculator.py
@@ -12,31 +12,56 @@ def MULTIPLY(a, b):
     return a * b
 
 def DIVIDE(a, b):
+    if (b == 0):
+        return "-ERROR DETECTED! DO NOT ENTER 0, YOU CANNOT DIVIDE BY 0"
     return a / b
 
 def main():
+
     print("HELLO, I AM YOUR CALCULATOR BOT, HERE TO ASSIST YOU WITH YOUR CALCULATIONS.")
-
-    oper = input("WILL YOU ADD, SUBTRACT, MULTIPLY, OR DIVIDE? ")
-
-    print("IF YOU'RE SUBTRACTING, THE SECOND NUMBER YOU INPUT WILL BE TAKEN OUT FROM THE FIRST NUMBER.")
-    print("IF YOU'RE DIVIDING, THE SECOND NUMBER YOU INPUT WILL BE WHAT THE FIRST NUMBER IS DIVDED BY.")
-
-    a = int(input("WHAT IS YOUR FIRST NUMBER? "))
-    b = int(input("WHAT IS YOUR SECOND NUMBER? "))
     
     
-    if (oper == "add"):
-        ans = ADD(a,b)
-    
-    if (oper == "subtract"):
-        ans = SUBTRACT(a,b)
-    
-    if (oper == "multiply"):
-        ans = MULTIPLY(a,b)
-    
-    if (oper == "divide"):
-        ans = DIVIDE(a,b)
+    while True:
+        print('TYPE "QUIT" WHEN ASKED TO ENTER AN OPERATOR TO STOP THE CALCULATOR')
+        oper = input("WILL YOU ADD, SUBTRACT, MULTIPLY, OR DIVIDE? ").lower()
         
-    print("YOUR ANSWER IS " + str(ans))
+        if (oper == "quit"):
+            print('OPTION "QUIT" SELECTED, I HOPE I HAVE ASSISTED YOU!')
+            break 
+        if oper not in ("add", "subtract", "multiply", "divide", " "):
+            print('PLEASE ENTER "ADD", "SUBTRACT", "MULTIPLY", OR "DIVIDE".')
+            continue
+        
+        print(" ")
+        
+        print("IF YOU'RE SUBTRACTING, THE SECOND NUMBER YOU INPUT WILL BE TAKEN OUT FROM THE FIRST NUMBER.")
+        print("IF YOU'RE DIVIDING, THE SECOND NUMBER YOU INPUT WILL BE WHAT THE FIRST NUMBER IS DIVDED BY.")
+
+        try:
+            a = float(input("WHAT IS YOUR FIRST NUMBER? "))
+        except ValueError:
+            print("PLEASE ENTER A NUMBER, NOTHING ELSE.")
+            continue
+
+        try:
+            b = float(input("WHAT IS YOUR SECOND NUMBER? "))
+        except ValueError:
+            print("PLEASE ENTER A NUMBER, NOTHING ELSE.")
+            continue
+        
+        if (oper == "add"):
+            ans = ADD(a,b)
+        
+        elif (oper == "subtract"):
+            ans = SUBTRACT(a,b)
+        
+        elif (oper == "multiply"):
+            ans = MULTIPLY(a,b)
+        
+        elif (oper == "divide"):
+            ans = DIVIDE(a,b)
+            
+        print(f"YOUR ANSWER IS {ans}")
+
 main()
+
